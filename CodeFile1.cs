8075308e76eb005cfbc7a959377f04cf6219aab7
Vitor4818 <vitorg.martins4818@gmail.com>
2025-05-05 00:51:31 -0300

    chore: iniciar projeto

---
+++ CodeFile1.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+class Musica
+{
+    public string nome { get; set; }
+    public string artista { get; set; }
+    public int duracao { get; set; } 
+    public bool disponivel { get; set; }
+    public string Descricao => $"A musica {nome} pertence a banda {nome}";
+
+
+    public void exibirFichaTecnica()
+    {
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Artista: " + artista);
+        Console.WriteLine("Duração: " + duracao + " segundos");
+        Console.WriteLine("Disponível: " + (disponivel ? "Sim" : "Não"));
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Fake Plastic Trees";
+musica1.artista = "Radiohead";
+musica1.duracao = 240;
+musica1.disponivel = true;
+Console.WriteLine("Ficha técnica da música:");
+musica1.exibirFichaTecnica();
